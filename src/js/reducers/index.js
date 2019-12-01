12f392e4ed0fc1145ed0733b36fb48f8f4444367
Taylor Blocker <taylorblocker@gmail.com>
2019-12-01 16:49:12 -0600

    comitting changes to made to multiple forms in set up of react-redux

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,12 +1,12 @@
 import { ADD_ARTICLE } from '../constants/action-types';
 
 const initialState = {
-	articles: [],
+	articles: []
 };
 function rootReducer(state = initialState, action) {
 	if (action.type === ADD_ARTICLE) {
 		return Object.assign({}, state, {
-			articles: state.articles.push(action.payload),
+			articles: state.articles.concat(action.payload),
 		});
 	}
 	return state;
