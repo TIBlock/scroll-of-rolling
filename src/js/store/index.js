da2604c1b1c461b7ba9ef4d70ec2d3f29f94bd99
Taylor Blocker <taylorblocker@gmail.com>
2019-12-01 20:58:48 -0600

    completed looking at middleware and its implementation and moving on to processing calls from an API

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
-const store = createStore(rootReducer);
+import {forbiddenWordsMiddleware} from "../middleware"
+
+const store = createStore(
+    rootReducer, 
+    applyMiddleware(forbiddenWordsMiddleware)
+);
 export default store;
 
